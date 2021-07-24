a97139b6809ba1442f27f91e82a668617f9aa777
ZornStolz <juan.vera1@correo.icesi.edu.co>
2021-07-23 19:11:44 -0500

    first steps on react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { AppContextWrapper } from "./store/AppContext";
+import TaskContainer from "./components/TaskContainer/TaskContainer";
+
+// function App () {
+//   return <p> Hello world David! </p>;
+// }
+// mismo que lo de abajo
 
 const App = () => {
-  return <p>Hello world!</p>;
+  return (
+    <AppContextWrapper>
+      <TaskContainer />
+    </AppContextWrapper>
+  );
 };
 
 export default App;
---
+++ src/components/TaskContainer/TaskContainer.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useContext } from "react";
+import axios from "axios";
+import AppContext from "../../store/AppContext";
+
+const TaskContainer = () => {
+  const state = useContext(AppContext);
+
+  //llamado a backend
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const loadTask = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      //console.log(response.data);
+      const tasks = response.data.slice(0, 5);
+      console.log(tasks);
+      state.setTasks(tasks);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    //console.log("David");
+    loadTask();
+  }, [loadTask]);
+  return <p> TaskContainer </p>;
+};
+
+export default TaskContainer;
